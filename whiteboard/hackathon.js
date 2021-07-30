adb902f28647dac40f0c4f754937e0d64d14258a
Palaksharma-dotcom <70392674+Palaksharma-dotcom@users.noreply.github.com>
2021-07-31 02:10:05 +0530

    Update hackathon.js

--- whiteboard/hackathon.js
+++ whiteboard/hackathon.js
@@ -1,21 +1,105 @@
 const canvas=document.getElementById("canvas");
-canvas.width=window.innerWidth-60;
-canvas.height=400;
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 
 let context=canvas.getContext("2d");
-let start_background_color="white";
+let start_background_color="gray";
 context.fillStyle=start_background_color;
 context.fillRect(0,0,canvas.width,canvas.height);
 
 let draw_color="black"; /*may change color of pen from here*/
 let draw_width="2";
 let is_drawing=false;
-
+var a =0
+var  b =0
 let restore_array=[];
 let index=-1;
+// function change_color(element) {
+//     draw_color=element.style.background;
+//     // document.getElementsByClassName('color-picker').style.shadowbox=this.draw_color ;
+//     // console.log(this) ;
+// }
+function rectanglle(){
+    move("rectangle")
+   }
+   function circlee(){
+       move("circle")
+   }
+   
+   
+   function rec(){
+       
+       var alpha=document.querySelector('.colorf1');
+       if(alpha.classList.contains('hide'))
+       {
+           alpha.classList.remove('hide');
+       }
+       else{
+           alpha.classList.add('hide');
+       }
+   
+   
+   }
+   
+   
+   function cir(){
+       
+       var alpha=document.querySelector('.colorf2');
+       if(alpha.classList.contains('hide'))
+       {
+           alpha.classList.remove('hide');
+       }
+       else{
+           alpha.classList.add('hide');
+       }
+   
+   
+   }
+   
+var dragValue ;
+
+function move(id){
+    console.log(id);
+  var element = document.getElementById(id);
+  console.log(element)
+  element.style.position = "absolute";
+   element.onmousedown = function(){
+       dragValue = element ;
+   }
+
+document.onmouseup = function(e){
+dragValue = null;
+}
+document.onmousemove = function(e){
+    var x = e.pageX;
+    var y = e.pageY;
+
+    dragValue.style.left = x + "px";
+    dragValue.style.top = y + "px";
+
+}
+}
+
+
+
+
+function change_backgroundcolor(element){
+    console.log(element)
+    // start_background_color=element.style.background;
+    console.log(start_background_color);
+    context.fillStyle=start_background_color;
+    context.fillRect(0,0,canvas.width,canvas.height);
+}
 
 function change_color(element) {
     draw_color=element.style.background;
+    draw_width="2";
+}
+function eraser(element) {
+    // element.preventDefault();
+    draw_color=start_background_color;
+    draw_width= "58";
+    // element.style.z-index = 5px;
 }
 canvas.addEventListener("touchstart",start,false);  /* for mobile and tablet versions */
 canvas.addEventListener("touchmove" ,draw,false);  /* for mobile and tablet versions */
@@ -56,13 +140,13 @@ function  stop(event) {
         context.closePath();
         is_drawing=false;
     }
-    event.preventDefault();
+    
 
     if(event.type!='mouseout'){
     restore_array.push(context.getImageData(0,0,canvas.width,canvas.height));
     index+=1;
     }
-    
+    event.preventDefault();
 }
 
 
@@ -87,3 +171,14 @@ function undo_last() {
     }
     
 }
+document.getElementById("open-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.add("active");
+  });
+   
+  document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.remove("active");
+  });
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
